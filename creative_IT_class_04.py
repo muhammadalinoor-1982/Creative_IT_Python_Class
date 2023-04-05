7c66c0a76205e4d3a472646d999d0db072c6bac8
muhammadalinoor-1982 <noor.web.journey@gmail.com>
2023-04-05 16:39:50 +0600

    Class No: 04 Done at 05/04/2023

---
+++ creative_IT_class_04.py
@@ -0,0 +1,36 @@
+""" Condition sytex:
+if statement:
+    pass
+elif statement:
+    pass
+else:
+    pass """
+
+""" a = int(input('Enter Your Number: '))
+
+if a>10:
+    print('a is graterthan 10')
+elif a<10:
+    print('a is lessthan 10')
+else:
+    print('a is equal 10')
+ """
+marks = float(input('Enter Your marks: '))
+
+if marks>=90 and marks<=100:
+    print('Your Grade Point is A+')
+elif marks>80 and marks<=89:
+    print('Your Grade Point is A')
+elif marks>70 and marks<=79:
+    print('Your Grade Point is A-')
+elif marks>60 and marks<=69:
+    print('Your Grade Point is B')
+elif marks>50 and marks<=59:
+    print('Your Grade Point is C')
+elif marks<50:
+    print('You are Fail')
+else:
+    print('Invalid input..!!')
+
+
+
