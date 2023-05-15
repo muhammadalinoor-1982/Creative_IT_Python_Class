cb9a26cffdc28b3cd64b9a8863688b0ef54dac67
muhammadalinoor-1982 <noor.web.journey@gmail.com>
2023-05-15 20:20:45 +0600

    Django Super user password and App creation Date: 15/05/2023

--- CIT_class_09_DJango.py
+++ CIT_class_09_DJango.py
@@ -1,2 +1,12 @@
-# Creative IT Class #09 Topic: Django Date: 15/05/2023
+# Creative IT Class #09 Topic: Django Super user password and App creation Date: 15/05/2023
+# To freeze server >>> pip freeze
+# To migrate table in database >> python manage.py makemigrations
+# To run migration >> python manage.py migrate
+# Go to admin panel >> http://127.0.0.1:8000/admin/login/?next=/admin/
+# To Create user >> python manage.py createsuperuser
+# Enter your user name
+# Enter your email
+# Enter your password
+# Confirm your password
+# To Create your App >> python manage.py startapp App_Name
 
---
+++ CIT_class_Authors.py
@@ -0,0 +1,22 @@
+'''
+App
+Secret Id-crju24
+
+Zoom ID: 931 0569 4322
+New ID: 934 6670 7235
+
+Class coordinator Rezowana apu-01990779775 (Thursday off)
+
+Hotline-01990779888
+
+(DH) Farzana apu email-   farzana.cit.bd@gmail.com  (Head of Quality)
+
+Md Sydul Amin
+E-Mail : sydulamin6@gmail.com
+Phone : 01641879498
+Skype : live:sydulamin6
+
+Class Video Google Drive Link: https://l.facebook.com/l.php?u=https%3A%2F%2Fdrive.google.com%2Fdrive%2Ffolders%2F1lkERG95dEzAyAVuvMe6GEuvGzndBMWvv%3Fusp%3Dsharing%26fbclid%3DIwAR3w8l106NUOFYpZhiHaR-6lLfmDsBPwvQB2_n_cZ_PrlZBy3_i1BQbaJak&h=AT23pE4MYpcjkNlcnVxeZoAO4nh4SJhvASmN3bdzFn2DznTOWuxZ9YfiYksI8wEQYOgfe7TMbx8-OF6fclC9konWxrnzaVl4Xwgd7AaoDIARS7pPYlnCdiz_5FZRuNUO-gSiag
+
+https://platform.futurenation.gov.bd/
+'''
