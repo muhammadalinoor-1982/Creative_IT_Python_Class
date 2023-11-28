1aea8cce0b1c65555cc57a488abffbb22ee25aa3
Muhammad Ali Noor <49801512+muhammadalinoor-1982@users.noreply.github.com>
2023-11-28 19:10:42 +0600

    Print Star Pyramid updated

--- CIT_class_05.py
+++ CIT_class_05.py
@@ -27,11 +27,15 @@ for j in range(0,11,2):
     print("\n")
 
 # Print Star Pyramid
-star = int(input("Enter number of star: "))
-for k in range(star):
-    for l in range(k+1):
-        print("* ", end="")
-    print("\n")
+star = int(input('Enter Number: '))
+k = star-1
+for i in range(0, star):
+    for j in range(0, k):
+        print(end=' ')
+    k = k - 1
+    for j in range(0, i+1):
+        print('* ', end='')
+    print('\r')
 
 # Print Star with * Pyramid
 for z in range(1,8):
